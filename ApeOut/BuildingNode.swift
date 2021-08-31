013cd66152da32eb9c66aa02ec3125da8b9930a5
AtinAgnihotri <atinagnihotri@gmail.com>
2021-08-31 12:55:20 +0530

    Adds BuildingNode + Building generation

--- ApeOut/BuildingNode.swift
+++ ApeOut/BuildingNode.swift
@@ -5,8 +5,68 @@
 //  Created by Atin Agnihotri on 31/08/21.
 //
 
+import SpriteKit
 import UIKit
 
-class BuildingNode: SKSpriteNode {
 
+
+class BuildingNode: SKSpriteNode {
+    
+    var currentImage: UIImage!
+    
+    func setup() {
+        name = "building"
+        currentImage = drawBuilding(of: size)
+        texture = SKTexture(image: currentImage)
+        
+        configurePhysics()
+    }
+    
+    func configurePhysics() {
+        physicsBody = SKPhysicsBody(texture: texture!, size: size)
+        physicsBody?.isDynamic = false
+        physicsBody?.categoryBitMask = CollisionTypes.building.rawValue
+        physicsBody?.contactTestBitMask = CollisionTypes.banana.rawValue
+    }
+    
+    func drawBuilding(of size: CGSize) -> UIImage {
+        let renderer = UIGraphicsImageRenderer(size: size)
+        
+        let renderedImage = renderer.image { context in
+            let cgContext = context.cgContext
+            
+            let rectangle = CGRect(origin: CGPoint(x: 0, y: 0), size: size)
+            
+            let color: UIColor
+            
+            // Select random color for building
+            switch Int.random(in: 0...2) {
+            case 0:
+                color = UIColor.BuildingColors.firstColor
+            case 1:
+                color = UIColor.BuildingColors.secondColor
+            default:
+                color = UIColor.BuildingColors.defaultColor
+            }
+            
+            // Draw building
+            color.setFill()
+            cgContext.addRect(rectangle)
+            cgContext.drawPath(using: .fill)
+            
+            // Draw windows with lights on or off at random
+            for row in stride(from: 10, to: Int(size.height - 10), by: 40) {
+                for col in stride(from: 10, through: Int(size.width - 10), by: 40) {
+                    if Bool.random() {
+                        UIColor.WindowColors.lightOnColor.setFill()
+                    } else {
+                        UIColor.WindowColors.lightOffColor.setFill()
+                    }
+                    cgContext.fill(CGRect(x: col, y: row, width: 15, height: 20))
+                }
+            }
+        }
+        
+        return renderedImage
+    }
 }
--- ApeOut/GameScene.swift
+++ ApeOut/GameScene.swift
@@ -9,8 +9,45 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    let BUILDING_LEFT_EDGE: CGFloat = -15
+    let BUILDING_RIGHT_EDGE: CGFloat = 1024
+    
+    var BUILDING_RANDOM_WIDTH: Int {
+        Int.random(in: 2...4) * 40
+    }
+    var BUILDING_RANDOM_HEIGHT: Int {
+        Int.random(in: 300...600)
+    }
+    
+    var buildings = [BuildingNode]()
+    
     override func didMove(to view: SKView) {
-
+        setupEnvironment()
+        print("Game loaded")
+    }
+    
+    func setupEnvironment() {
+        // Setup background
+        backgroundColor = UIColor.BackgroundColor.backgroundColor
+        
+        createBuildings()
+    }
+    
+    func createBuildings() {
+        var currentX = BUILDING_LEFT_EDGE
+        
+        while currentX < BUILDING_RIGHT_EDGE {
+            let size = CGSize(width: BUILDING_RANDOM_WIDTH, height: BUILDING_RANDOM_HEIGHT)
+            currentX += size.width + 2 // Little bit spacing bw building
+            let buildingPosition = CGPoint(x: currentX - (size.width / 2), y: size.height / 2)
+            
+            let building = BuildingNode(color: .red, size: size)
+            building.position = buildingPosition
+            building.setup()
+            addChild(building)
+            
+            buildings.append(building)
+        }
     }
     
     override func update(_ currentTime: TimeInterval) {
